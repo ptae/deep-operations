6c551d1dbb11d8d3876495337d6a21243ed9dac6
gabrielSeixas <salesseixasgabriel@gmail.com>
2018-04-04 12:04:12 -0300

    Add semicolons on merge source

--- src/internals/merge.ts
+++ src/internals/merge.ts
@@ -5,17 +5,17 @@ import {
   unique,
   normalize,
   containsOnlyPrimitives
-} from './../utils'
-import { Options } from '../types'
+} from './../utils';
+import { Options } from '../types';
 
 const parseArraysToIndexObjects = (arrayOne: any[], arrayTwo: any[], indexKey: string): any[] => {
-  return [normalize(arrayOne, indexKey), normalize(arrayTwo, indexKey)]
-}
+  return [normalize(arrayOne, indexKey), normalize(arrayTwo, indexKey)];
+};
 
 const mergeObjectsOptions: Options = {
   mergeObjectIntoArrays: false,
   indexKeyOnArrays: ''
-}
+};
 
 /**
  * It will merge only two objects
@@ -23,42 +23,42 @@ const mergeObjectsOptions: Options = {
 export const deepMergeTwoObjects = (
   objOne: any,
   objTwo: any,
-  options = mergeObjectsOptions
+  options: Options = mergeObjectsOptions
 ): object => {
-  const { indexKeyOnArrays, mergeObjectIntoArrays } = options
-  const objOneKeys = Object.keys(objOne)
-  const objTwoKeys = Object.keys(objTwo)
+  const { indexKeyOnArrays, mergeObjectIntoArrays } = options;
+  const objOneKeys = Object.keys(objOne);
+  const objTwoKeys = Object.keys(objTwo);
 
   return objTwoKeys.reduce((accumulator, currKey) => {
-    const oneHasKey = objOneKeys.includes(currKey)
+    const oneHasKey = objOneKeys.includes(currKey);
 
-    const isArray = isBothArray(objOne, objTwo, currKey)
-    const isObject = isBothObject(objOne, objTwo, currKey)
+    const isArray = isBothArray(objOne, objTwo, currKey);
+    const isObject = isBothObject(objOne, objTwo, currKey);
 
-    const concatArrays = () => unique(objOne[currKey].concat(objTwo[currKey]))
-    const mergeObject = () => isObject && deepMergeTwoObjects(objOne[currKey], objTwo[currKey])
+    const concatArrays = () => unique(objOne[currKey].concat(objTwo[currKey]));
+    const mergeObject = () => isObject && deepMergeTwoObjects(objOne[currKey], objTwo[currKey]);
 
     const concatOrMerge = () =>
       mergeObjectIntoArrays &&
       !containsOnlyPrimitives(objOne[currKey]) &&
       !containsOnlyPrimitives(objTwo[currKey])
         ? Object.values(mergeArrayObjects(objOne[currKey], objTwo[currKey], indexKeyOnArrays))
-        : concatArrays()
+        : concatArrays();
 
-    const mergedObject = isArray ? concatOrMerge() : mergeObject()
+    const mergedObject = isArray ? concatOrMerge() : mergeObject();
 
-    const result = isPrimitive(objTwo[currKey]) ? objTwo[currKey] : mergedObject
-    const putIfNew = !oneHasKey ? objTwo[currKey] : result
+    const result = isPrimitive(objTwo[currKey]) ? objTwo[currKey] : mergedObject;
+    const putIfNew = !oneHasKey ? objTwo[currKey] : result;
 
-    return { ...accumulator, [currKey]: putIfNew }
-  }, {})
-}
+    return { ...accumulator, [currKey]: putIfNew };
+  }, {});
+};
 
 const mergeArrayObjects = (arrayOne: any[], arrayTwo: any[], indexKey: string) => {
-  const [indexedOne, indexedTwo] = parseArraysToIndexObjects(arrayOne, arrayTwo, indexKey)
+  const [indexedOne, indexedTwo] = parseArraysToIndexObjects(arrayOne, arrayTwo, indexKey);
 
   return deepMergeTwoObjects(indexedOne, indexedTwo, {
     indexKeyOnArrays: indexKey,
     mergeObjectIntoArrays: true
-  })
-}
+  });
+};
